c7b0d62d93e166096fbf602e5c23cf10af04c2e4
Manuel Bonini <manuelbonini17@gmail.com>
2021-04-07 16:06:56 +0200

    non funziona l'ultima parte, da sistemare

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// Generatore dei numeri
+var numbers = [];
+// puo generare numeri tra 1 e maxNumeri
+var maxNumeri = 100;
+// totale dei numeri da memorizzare
+var totNumbers = 5;
+
+// Ciclo while finchè non riempie l'array con 5 numeri che non siano uguali
+while(numbers.length < totNumbers) {
+    var numberGenerated = numeroCasuale(maxNumeri);
+    if(numbers.includes(numberGenerated) == false) {
+        numbers.push(numberGenerated);
+    }
+}
+
+// funzione che genera un numero casuale
+function numeroCasuale(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+// console.log(numbers);
+
+// alert con i 5 numeri
+alert('I numeri da memorizzare sono:' + numbers);
+
+// creo un timer di 30 secondi 
+var milliseconds = 3000;
+var userNumbers = [];
+
+setTimeout(askUser, milliseconds);
+
+function askUser() {
+    // chiedo all'utente di inserire i 5 numeri dopo 30 secondi
+    var userNumbers = [];
+    var number = 0;
+
+    while(userNumbers.length < totNumbers) {
+        number = parseInt(prompt('Inserisci un numero'));
+        userNumbers.push(number);
+        // console.log(userNumbers);
+    }
+    console.log(userNumbers);
+
+    var guessNumbers = [];
+    var sum = 0;
+    for( i = 0 ; i < totNumbers ; i++) {
+        if(numbers.includes(userNumbers) == true) {
+            guessNumbers.push(numbers);
+            sum++;
+            console.log(guessNumbers);
+        }
+    }
+
+    if(guessNumbers == totNumbers) {
+        alert('Hai indovinato tutti i numeri e sono: ' + guessNumbers);
+
+    }else {
+        alert('Hai indovintao ' + sum + ' e sono: ' + guessNumbers);
+    }
+}
